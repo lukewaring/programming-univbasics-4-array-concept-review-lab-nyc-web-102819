77337d9e3506fce50e14b62a4b51c742c53a82f7
Luke Waring <lukewaring@gmail.com>
2019-10-09 19:11:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,10 +14,11 @@ end
 
 def find_max_value(array)
   max = 0 
-  array.length.times do |index|
-  if array[index] > max
-  max = array[index]
-
+  array.length.times { |index|
+    if array[index] > max
+      max = array[index]
+    end
+  }
 end
 
 def find_min_value(array)
