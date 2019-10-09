5693c2d71deb1c26c9ce685110e27006c34b40bd
Luke Waring <lukewaring@gmail.com>
2019-10-09 18:32:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index do |i| 
+    i == value_to_find
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
+
+
+#scale = [2,3,4,5]
+#scale_2 = [6,5,4,3]
+
+#array.length.times do |index|
+#  puts array[index]
+#end
